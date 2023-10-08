0c0e0c671029cc00e6492b2ec970f0a14e959837
omidashtari <oashtari@gmail.com>
2023-10-08 06:28:30 -0700

    added routes for the CRUD methods

---
+++ src/router.rs
@@ -0,0 +1,25 @@
+pub async fn create_router(dbpool: sqlx::Pool<sqlx::Sqlite>) -> axum::Router {
+    use create::api::{ping, todo_create, todo_delete, todo_list, todo_read, todo_update};
+
+    use axum::{routing::Get, Router};
+
+    use tower_http::cors::{Any, CorsLayer};
+
+    use tower_http::trace::TraceLayer;
+
+    Router::new()
+        .route("/alive", get(|| async { "ok" }))
+        .route("/ready", get(ping))
+        .nest(
+            "/v1",
+            Router::new()
+                .route("/todos", get(todo_list).post(create_todo))
+                .route(
+                    "/todos/:id",
+                    get(todo_read).put(todo_update).delete(todo_delete),
+                ),
+        )
+        .with_state(dbpool)
+        .layer(CorsLayer::new().allow_methods(Any).allow_origin(Any))
+        .layer(TraceLayer::new_for_http())
+}
