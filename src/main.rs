4e30a164d26fbdc9b18815380b9805f4491ef26b
omidashtari <oashtari@gmail.com>
2023-10-10 05:10:10 -0700

    finished CLI code

--- src/main.rs
+++ src/main.rs
@@ -63,10 +63,7 @@ async fn request(
                 .uri(url)
                 .method(method)
                 .header("Content-Type", "application/json")
-                .body(
-                    body.map(|s| Body::from(s))
-                        .unwrap_or_else(|| Body::empty()),
-                )?,
+                .body(body.map(|s| Body::from(s)).unwrap_or_else(|| Body::empty()))?,
         )
         .await?;
 
@@ -136,7 +133,6 @@ async fn request(
 
 // }
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let cli = Cli::parse();
@@ -148,20 +144,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     }
 
     if let Some(authority) = cli.url.authority() {
-        uri_builder = uri_builder.authority(authority.cloen());
+        uri_builder = uri_builder.authority(authority.clone());
     }
 
     match cli.command {
         Commands::List => {
             request(
-                uri_builder.path_and_query("/v1/todos").build()?;
+                uri_builder.path_and_query("/v1/todos").build()?,
+                Method::GET,
+                None,
             )
             .await
         }
         Commands::Delete { id } => {
             request(
-                uri_builder.path_and_query(foamat!("/v1/todos/{}", id))
-                .build()?,
+                uri_builder
+                    .path_and_query(format!("/v1/todos/{}", id))
+                    .build()?,
                 Method::DELETE,
                 None,
             )
@@ -169,7 +168,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         }
         Commands::Read { id } => {
             request(
-                uri_builder.path_and_query(format!("/v1/todos/{}", id)).build()?,
+                uri_builder
+                    .path_and_query(format!("/v1/todos/{}", id))
+                    .build()?,
                 Method::GET,
                 None,
             )
@@ -183,78 +184,85 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             )
             .await
         }
-        Commands::Update { id, body, completed } => {
+        Commands::Update {
+            id,
+            body,
+            completed,
+        } => {
             request(
-            uri_builder.path_and_query(format!("/v1/todos/{}", id)).build()?,
-            Method::PUT,
-            Some(json!({"body":body,"completed":completed}).to_string()),
-        )
-        .await
+                uri_builder
+                    .path_and_query(format!("/v1/todos/{}", id))
+                    .build()?,
+                Method::PUT,
+                Some(json!({"body":body,"completed":completed}).to_string()),
+            )
+            .await
         }
     }
-    // Initializes the tracing and logging for our service and its dependencies.
-    init_tracing();
 
-    // Initialized DB pool
-    let dbpool = init_dbpool().await.expect("couldn't initialize db pool");
+    //     // Initializes the tracing and logging for our service and its dependencies.
+    //     init_tracing();
 
-    // Creates the core application service and its routes.
-    let router = create_router(dbpool).await;
+    //     // Initialized DB pool
+    //     let dbpool = init_dbpool().await.expect("couldn't initialize db pool");
 
-    // Fetches the binding address from the environment variable BIND_ADDR, or uses the default value of 127.0.0.1:3000.
-    let bind_addr = std::env::var("BIND_ADDR").unwrap_or_else(|_| "127.0.0.1:3000".to_string());
+    //     // Creates the core application service and its routes.
+    //     let router = create_router(dbpool).await;
 
-    // Parses the binding address into a socket address.
-    // Creates the service and starts the HTTP server.
-    axum::Server::bind(&bind_addr.parse().unwrap())
-        .serve(router.into_make_service())
-        .await
-        .expect("unable to start server")
-}
+    //     // Fetches the binding address from the environment variable BIND_ADDR, or uses the default value of 127.0.0.1:3000.
+    //     let bind_addr = std::env::var("BIND_ADDR").unwrap_or_else(|_| "127.0.0.1:3000".to_string());
 
-fn init_tracing() {
-    use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
+    //     // Parses the binding address into a socket address.
+    //     // Creates the service and starts the HTTP server.
+    //     axum::Server::bind(&bind_addr.parse().unwrap())
+    //         .serve(router.into_make_service())
+    //         .await
+    //         .expect("unable to start server")
+    // }
 
-    // Fetches the RUST_LOG environment variable, providing a default value if it’s not defined.
+    // fn init_tracing() {
+    //     use tracing_subscriber::{filter::LevelFilter, fmt, prelude::*, EnvFilter};
 
-    let rust_log = std::env::var(EnvFilter::DEFAULT_ENV)
-        .unwrap_or_else(|_| "sqlx=info,tower_http=debug,info".to_string());
+    //     // Fetches the RUST_LOG environment variable, providing a default value if it’s not defined.
 
-    // Returns the default global registry.
-    // Adds a formatting layer, which provides human-readable trace formatting.
-    // Constructs an environment filter, with the default log level set to info, otherwise using the value provided by RUST_LOG.
-    tracing_subscriber::registry()
-        .with(fmt::layer())
-        .with(
-            EnvFilter::builder()
-                .with_default_directive(LevelFilter::INFO.into())
-                .parse_lossy(rust_log),
-        )
-        .init();
-}
+    //     let rust_log = std::env::var(EnvFilter::DEFAULT_ENV)
+    //         .unwrap_or_else(|_| "sqlx=info,tower_http=debug,info".to_string());
 
-async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Error> {
-    use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
-    use std::str::FromStr;
+    //     // Returns the default global registry.
+    //     // Adds a formatting layer, which provides human-readable trace formatting.
+    //     // Constructs an environment filter, with the default log level set to info, otherwise using the value provided by RUST_LOG.
+    //     tracing_subscriber::registry()
+    //         .with(fmt::layer())
+    //         .with(
+    //             EnvFilter::builder()
+    //                 .with_default_directive(LevelFilter::INFO.into())
+    //                 .parse_lossy(rust_log),
+    //         )
+    //         .init();
+    // }
 
-    // We’ll try to read the DATABASE_URL environment variable, or default to sqlite:db.sqlite
-    // if not defined (which opens a file called db.sqlite in the current working directory)
+    // async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Error> {
+    //     use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
+    //     use std::str::FromStr;
 
-    let db_connection_str =
-        std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite:db.sqlite".to_string());
+    //     // We’ll try to read the DATABASE_URL environment variable, or default to sqlite:db.sqlite
+    //     // if not defined (which opens a file called db.sqlite in the current working directory)
 
-    // When we connect to the database, we ask the driver to create the database if it doesn’t already exist.
-    let dbpool = SqlitePoolOptions::new()
-        .connect_with(SqliteConnectOptions::from_str(&db_connection_str)?.create_if_missing(true))
-        .await
-        .expect("can't connect to database");
+    //     let db_connection_str =
+    //         std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite:db.sqlite".to_string());
 
-    // After we’ve connected to the DB, we run any migrations that are needed.
-    // We can pass our newly created DB pool directly to SQLx, which will obtain a connection from the pool.
-    sqlx::migrate!()
-        .run(&dbpool)
-        .await
-        .expect("database migration failed.");
+    //     // When we connect to the database, we ask the driver to create the database if it doesn’t already exist.
+    //     let dbpool = SqlitePoolOptions::new()
+    //         .connect_with(SqliteConnectOptions::from_str(&db_connection_str)?.create_if_missing(true))
+    //         .await
+    //         .expect("can't connect to database");
 
-    Ok(dbpool)
+    //     // After we’ve connected to the DB, we run any migrations that are needed.
+    //     // We can pass our newly created DB pool directly to SQLx, which will obtain a connection from the pool.
+    //     sqlx::migrate!()
+    //         .run(&dbpool)
+    //         .await
+    //         .expect("database migration failed.");
+
+    //     Ok(dbpool)
 }
